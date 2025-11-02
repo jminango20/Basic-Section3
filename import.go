cc820b526063f3ff3cd2b5aa754c9c32b1fed940
Juan Minango <juan.minango@bbchain.com.br>
2025-11-02 16:04:50 -0300

    feat: import with alias

--- import.go
+++ import.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	foo "net/http"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	resp, err := foo.Get("https://jsonplaceholder.typicode.com/posts/1")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
